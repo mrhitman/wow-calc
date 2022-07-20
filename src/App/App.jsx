e63097e03642fefd96f89edc49a704271927a30e
Viktor Varenik <icviktor.varenik@americanwell.com>
2022-07-20 14:25:56 +0300

    add default route

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -5,7 +5,13 @@ import React from "react";
 import {useState, WowCalculatorContext} from "../store";
 import ClassSpecs from "../ClassSpecs/ClassSpecs";
 import ClassPicker from "../ClassPicker/ClassPicker";
-import {BrowserRouter as Router, Route, Routes, Link} from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import {classes} from "../store/classes";
 
 function App() {
   const [state, dispatch] = useState();
@@ -13,14 +19,13 @@ function App() {
   return (
     <WowCalculatorContext.Provider value={{state, dispatch}}>
       <Router>
-        <div>
-          <Link to="/picker">picker</Link>
-          <Link to="/">specs</Link>
-        </div>
         <ClassPicker />
         <Routes>
-          <Route path="/picker" element={<ClassPicker />} />
           <Route path="/:className" element={<ClassSpecs />} />
+          <Route
+            path="/"
+            element={<Navigate to={`/${classes[0].name}`} replace />}
+          />
         </Routes>
       </Router>
     </WowCalculatorContext.Provider>
